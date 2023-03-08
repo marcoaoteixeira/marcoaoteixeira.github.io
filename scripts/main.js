02709938e645bbc355392c3ab0b4de97b16f314d
Marco Teixeira <mteixeira@cardlytics.com>
2023-03-08 18:56:59 -0500

    fix language and add flags

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@
     i18n
       .load('./i18n/' + i18n.locale + '.json', i18n.locale)
       .done(function () {
-        alert($.i18n('About Me'))
+        $('body').i18n();
       });
   };
 
@@ -16,10 +16,10 @@
       anchorPlacement: 'top-left',
       duration: 1000
     });
-    
-    // $('.change-language').on('click', function () {
-    //     changeLanguage(this.lang.toLowerCase());
-    //     return false;
-    // });
+
+    $('.change-language').on('click', function () {
+        changeLanguage(this.lang.toLowerCase());
+        return false;
+    });
   });
 })(jQuery);
